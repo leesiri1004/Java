4f8197a5a321eee34e17123b44e9199cbfff5f7c
leesiri1004 <leesiri1004@gmail.com>
2020-11-10 12:18:44 +0900

    2020/11/10 Ch02 등록

---
+++ Ch02/src/sub1/HelloJava.java
@@ -0,0 +1,14 @@
+package sub1;
+
+/**
+ * 날짜 : 2020/11/02
+ * 이름 : 이슬이
+ * 내용 : Ch02. 자바 설치 예제 교재 p31
+ */
+public class HelloJava {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+
+}
