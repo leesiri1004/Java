b19651950511179ad9231a32ccd46bd8529eb002
leesiri1004 <leesiri1004@gmail.com>
2020-11-10 12:17:45 +0900

    2020/11/10 Ch01 등록

---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,16 @@
+package sub1;
+
+/*
+ * 날짜 : 2020/11/02
+ * 이름 : 이슬이
+ * 내용 : Ch01 자바 Hello World 출력하기
+ */
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+
+}
