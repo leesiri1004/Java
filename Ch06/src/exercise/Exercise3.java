acccc9aaf831306e2ffe2cb9397409c80727a6ff
leesiri1004 <leesiri1004@gmail.com>
2020-11-17 10:19:33 +0900

    2020/11/17 업데이트

--- Ch06/src/exercise/Exercise3.java
+++ Ch06/src/exercise/Exercise3.java
@@ -7,6 +7,10 @@ public class Exercise3 {
 		int totalScore = 0;
 		double avgScore = 0;
 		
+		for (int i = 0; i<score.length; i++) {
+			totalScore += score[i];
+		}
+		avgScore = (double)totalScore / score.length;
 		
 		
 		System.out.println("합계 점수 : "+totalScore);
