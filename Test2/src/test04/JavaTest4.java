0e1f66e23a8cd8228d890c463a93ec342fff39b4
leesiri1004 <leesiri1004@gmail.com>
2020-11-17 10:21:27 +0900

    2020/11/17 Test2 업데이트

--- Test2/src/test04/JavaTest4.java
+++ Test2/src/test04/JavaTest4.java
@@ -18,11 +18,11 @@ package test04;
 
 public class JavaTest4 {
 	public static void main(String[] args) {
-		
-		int arr[] = {4, 2, 1, 5, 3};
-		
-		for (int i=0; i<4; i++) {
-			for (int j=i+1; j<5; j++) {
+
+		int arr[] = { 4, 2, 1, 5, 3 };
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = i + 1; j < 5; j++) {
 				if (arr[i] > arr[j]) {
 					int temp = arr[j];
 					arr[j] = arr[i];
@@ -30,10 +30,10 @@ public class JavaTest4 {
 				}
 			}
 		}
-		
+
 		// 정렬된 배열 출력하기
-		for (int i=0; i<5; i++) {
-			System.out.print(arr[i]+", ");
+		for (int i = 0; i < 5; i++) {
+			System.out.print(arr[i] + ", ");
 		}
 	}
 
