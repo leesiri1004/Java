6b90f808a43c65e163f4b2da7af3e0afb3b8c452
leesiri1004 <leesiri1004@gmail.com>
2020-11-27 13:44:23 +0900

    2020/11/27 수업 예제 등록

---
+++ Ch19/src/sub1/HttpURLTest.java
@@ -0,0 +1,43 @@
+package sub1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/*
+ * 날짜 : 2020/11/24
+ * 이름 : 이슬이
+ * 내용 : HTTP 통신 실습하기 교재 p521
+ */
+
+public class HttpURLTest {
+
+	public static void main(String[] args) throws IOException {
+
+		URL url = new URL("https://www.google.co.kr/");
+
+		InputStreamReader isr = new InputStreamReader(url.openStream());
+		BufferedReader br = new BufferedReader(isr);
+
+		String line = null;
+
+		while (true) {
+
+			line = br.readLine();
+
+			if (line == null) {
+				break;
+			}
+
+			System.out.println(line);
+
+		}
+		
+		br.close();
+		
+		System.out.println("프로그램 종료...");
+
+	}
+}
